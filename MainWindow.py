bfe8fd99a0b17f5851daafabfcd864a969276b80
pratikKumarb18187 <b18187@students.iitmandi.ac.in>
2020-10-23 13:57:37 +0530

    renamed process.py to helpers.py moved function keyword_check to helpers.py

--- MainWindow.py
+++ MainWindow.py
@@ -1,7 +1,7 @@
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-from process import CountWords, PlotWordHistogram
+from helpers import CountWords, PlotWordHistogram
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 from PIL import Image
 import matplotlib.pyplot as plt
